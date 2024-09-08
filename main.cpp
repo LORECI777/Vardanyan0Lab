c0441450266c23c0282641e57eae073d983f0991
Vage Vardanyan <vardanyan5432@gmail.com>
2024-09-08 15:48:35 +0300

    second commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // Выводим строку "Hello world" на консоль
+    std::cout << "Hello world!" << std::endl;
+
+    // Запрашиваем ввод символа от пользователя
+    char symbol;
+    std::cout << "Input: ";
+    std::cin >> symbol;
+
+    return 0;
+}
